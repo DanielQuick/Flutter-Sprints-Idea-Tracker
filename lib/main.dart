f5816efebc92fa9253b470ceb3079672de7cac45
DanielQuick <danielquick14@gmail.com>
2021-01-08 19:50:33 -0600

    Add provider, locator, and related files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:idea_tracker/locator.dart';
 
 void main() {
+  setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
