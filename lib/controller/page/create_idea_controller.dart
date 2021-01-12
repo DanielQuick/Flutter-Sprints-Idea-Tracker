bbfc8a45f468d8b092980b3afe33c137c00a9f8e
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-11 23:17:31 -0300

    implement minimal functionality

--- lib/controller/page/create_idea_controller.dart
+++ lib/controller/page/create_idea_controller.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:idea_tracker/model/idea.dart';
 
 class CreateIdeaController extends ChangeNotifier {
-  int _counter;
-  int get counter => _counter;
-  set counter(int value) {
-    if (_counter == value) return;
-    _counter = value;
+  String _ideaTitle;
+  String _ideaDescription;
+
+  set ideaTitle(String title) {
+    _ideaTitle = title;
     notifyListeners();
   }
 
-  onIncrementFabPressed() {
-    counter++;
+  set ideaDescription(String description) {
+    _ideaDescription = description;
+    notifyListeners();
+  }
+
+  onCreateIdeaFabPressed() {
+    final _idea = Idea(
+      id: "1",
+      title: _ideaTitle,
+      description: _ideaDescription,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+    );
+    // TODO: POST idea throught Service
   }
 }
--- lib/view/page/create_idea_page.dart
+++ lib/view/page/create_idea_page.dart
@@ -4,38 +4,74 @@ import 'package:idea_tracker/controller/page/create_idea_controller.dart';
 
 class CreateIdeaPage extends StatelessWidget {
   final String title;
+  final _formKey = GlobalKey<FormState>();
 
   CreateIdeaPage({this.title});
 
   @override
   Widget build(BuildContext context) {
     return BaseView<CreateIdeaController>(
-      onModelReady: (controller) {
-        controller.counter = 10;
-      },
       builder: (context, controller, child) {
         return Scaffold(
           appBar: AppBar(
+            centerTitle: true,
             title: Text(title),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ),
-          body: Center(
+          body: Form(
+            key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'You have pushed the button this many times:',
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Idea Title",
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter idea title';
+                    }
+                    return null;
+                  },
+                  onChanged: (String value) {
+                    controller.ideaTitle = value;
+                  },
                 ),
-                Text(
-                  '${controller.counter}',
-                  style: Theme.of(context).textTheme.headline4,
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Idea Description",
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter idea description';
+                    }
+                    return null;
+                  },
+                  onChanged: (String value) {
+                    controller.ideaDescription = value;
+                  },
                 ),
               ],
             ),
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: controller.onIncrementFabPressed,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                controller.onCreateIdeaFabPressed();
+              }
+            },
+            label: Text('Create'),
+            icon: Icon(Icons.add),
+            tooltip: 'Create Idea',
           ),
         );
       },
