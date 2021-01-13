d6c567e4800b3760fa9755d3466b06d16f8e0c65
xopherw <xopher95@gmail.com>
2021-01-13 11:31:23 +0800

    made log out alert and profile status

--- lib/view/page/profile_page.dart
+++ lib/view/page/profile_page.dart
@@ -14,41 +14,82 @@ class _ProfilePageState extends State<ProfilePage> {
         actions: [
           PopupMenuButton(
             itemBuilder: (context) => [
-              PopupMenuItem(child: Text("Change Password")),
-              PopupMenuItem(child: Text("Log Out"))
+              PopupMenuItem(
+                child: Text("Change Password"),
+                value: 'Change Password',
+              ),
+              PopupMenuItem(
+                child: Text("Log Out"),
+                value: 'Log Out',
+              )
             ],
+            onSelected: (value) {
+              switch (value) {
+                case "Change Password":
+                  {
+                    print('change password');
+                  }
+                  break;
+                case "Log Out":
+                  {
+                    return showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Logging Out'),
+                          content: Text('Are you sure you want to log out?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Yes'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('No'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                  break;
+              }
+            },
             icon: Icon(Icons.more_vert_rounded),
           )
         ],
       ),
-      body: Column(
+      body: ListView(
         children: [
           Container(
             margin: EdgeInsets.all(30),
             height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 100,
+                  child: Text('# of sprints joined'),
+                ),
+                Container(
+                  width: 100,
+                  child: Text('# of times user has been team lead'),
+                )
+              ],
+            ),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
-              color: Colors.purple,
+              color: Colors.grey,
             ),
           ),
-          Column(
-            children: [
-              Text('Sprints Participant'),
-              Text('Team Lead'),
-              Text('Voted'),
-            ],
-          ),
+          Container(child: Text('Sprints Participant')),
+          Container(child: Text('Team Lead')),
+          Container(child: Text('Voted')),
         ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.orange,
-        child: Container(
-          height: 50,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [Text('Idea'), Text('Sprints'), Text('Profile')],
-          ),
-        ),
       ),
     );
   }
