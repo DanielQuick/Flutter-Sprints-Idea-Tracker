6e8596c18c0022bf5ae82c158b4eea15be4d6bbe
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-14 19:04:32 -0300

    only confirm delete in dialog

--- lib/controller/dialog/idea_edit_details_dialog_controller.dart
+++ lib/controller/dialog/idea_edit_details_dialog_controller.dart
@@ -4,9 +4,6 @@ class IdeaEditDetailsDialogController extends ChangeNotifier {
   Function() onConfirmDelete;
 
   void confirmDelete() async {
-    // TODO: delete idea throught IdeaService
-    await Future.delayed(Duration(milliseconds: 1000), () {});
     if (onConfirmDelete != null) onConfirmDelete();
-    print("Idea deleted");
   }
 }
