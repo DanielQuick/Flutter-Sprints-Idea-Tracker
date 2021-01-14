b9ba97a2bb08da76eaaac564d69cf4dd4bbb5d31
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-14 14:19:34 -0300

    register IdeaEditDetailsPageController

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:idea_tracker/controller/page/create_idea_page_controller.dart';
+import 'package:idea_tracker/controller/page/idea_edit_details_page_controller.dart';
 import 'package:idea_tracker/controller/page/main_page_controller.dart';
 
 GetIt locator = GetIt.instance;
@@ -7,4 +8,5 @@ GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerFactory(() => MainPageController());
   locator.registerFactory(() => CreateIdeaPageController());
+  locator.registerFactory(() => IdeaEditDetailsPageController());
 }
