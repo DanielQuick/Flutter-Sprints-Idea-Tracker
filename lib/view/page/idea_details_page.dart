2152635e5ab262fa900a33d8655ca916b266b2d7
Abdul-Aziz <applemafia69@yahoo.com.com>
2021-01-17 17:58:48 +0000

    Create Idea Details page

---
+++ lib/view/page/idea_details_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IdeaDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Idea Details Page'),
+        ),
+      ),
+    );
+  }
+}
