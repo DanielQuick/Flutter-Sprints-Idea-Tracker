e0fa45b396dcab62cf2a5c8c8ae1ae557806d72a
Abdul-Aziz <applemafia69@yahoo.com.com>
2021-01-17 19:09:09 +0000

    Update AppBar

--- lib/view/page/idea_details_page.dart
+++ lib/view/page/idea_details_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class IdeaDetailsPage extends StatelessWidget {
+  final String title;
+  final String description;
+
+  IdeaDetailsPage({this.title, this.description});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Idea Details Page'),
+          title: Text('$title Idea'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.pop(context),
+          ),
         ),
       ),
     );
