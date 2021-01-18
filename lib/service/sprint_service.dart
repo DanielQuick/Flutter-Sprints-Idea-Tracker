b6db6a68ec5d6425ebf591b7cfcdb6b49456a7f4
taracell <48975214+taracell@users.noreply.github.com>
2021-01-17 16:34:57 -0800

    Firestore finally working

--- lib/service/sprint_service.dart
+++ lib/service/sprint_service.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import '../model/models.dart';
+import '../model/sprint.dart';
 
 class SprintService {
   ///create variable instances for use
