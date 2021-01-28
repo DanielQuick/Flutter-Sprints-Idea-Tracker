82a3c3c6327f8f1f0c046e4df3f3a6c3d00f7ae2
labody <60046603+labody@users.noreply.github.com>
2021-01-28 10:23:52 +0000

    Show different UI when no ideas are available

--- lib/view/page/ideas_main_page.dart
+++ lib/view/page/ideas_main_page.dart
@@ -25,59 +25,33 @@ class IdeasMainPage extends StatelessWidget {
           },
           child: Icon(Icons.add),
         ),
-        body: controller.ideas.isEmpty ? NoIdeasPage() : IdeaCardsList(),
+        body: controller.ideas.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No ideas available now',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Tap the button below to create a new one!',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ],
+                ),
+              )
+            : ListView.builder(
+                itemCount: controller.ideas.length,
+                itemBuilder: (context, index) {
+                  return IdeaCard(
+                    idea: controller.ideas[index],
+                  );
+                },
+              ),
       );
     });
-  }
-}
-
-class IdeaCardsList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BaseView<IdeasMainPageController>(
-      onControllerReady: (controller) {
-        controller.initialize();
-      },
-      builder: (context, controller, child) {
-        return ListView.builder(
-          itemCount: controller.ideas.length,
-          itemBuilder: (context, index) {
-            return IdeaCard(
-              idea: controller.ideas[index],
-            );
-          },
-        );
-      },
-    );
-  }
-}
-
-class NoIdeasPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BaseView<IdeasMainPageController>(
-      onControllerReady: (controller) {
-        controller.initialize();
-      },
-      builder: (context, controller, child) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'No ideas available now',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                'Tap the button below to create a new one!',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ],
-          ),
-        );
-      },
-    );
   }
 }
 
