00f9defee3e23babfe8b9c7f9fe76f1896a7c659
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-29 14:02:12 -0300

    simplify UI to only consider 2 states and use controller.message

--- lib/view/dialog/show_landing_page_recover_password_dialog.dart
+++ lib/view/dialog/show_landing_page_recover_password_dialog.dart
@@ -8,37 +8,21 @@ Future showLandingPageRecoverPasswordDialog({
 }) async {
   return showDialog(
     context: context,
-    // User must press "Close" button. Eventually, this behavior will change
-    barrierDismissible: false,
     builder: (context) {
       return BaseView<LandingPageRecoverPasswordDialogController>(
         builder: (context, controller, child) {
           Widget alertDialogContent;
 
-          switch (controller.status) {
-            case RecoverPasswordStatus.initial:
-              alertDialogContent = _alertContentInitial(
-                context: context,
-                controller: controller,
-              );
-              break;
-            case RecoverPasswordStatus.waiting:
-              alertDialogContent = _alertContentWaiting(
-                context: context,
-              );
-              break;
-            case RecoverPasswordStatus.failed:
-              alertDialogContent = _alertContentFailed(
-                context: context,
-                controller: controller,
-              );
-              break;
-            case RecoverPasswordStatus.success:
-              alertDialogContent = _alertContentSuccess(
-                context: context,
-                controller: controller,
-              );
-              break;
+          if (controller.requestSubmited) {
+            alertDialogContent = _alertContentFinish(
+              context: context,
+              controller: controller,
+            );
+          } else {
+            alertDialogContent = _alertContentInitial(
+              context: context,
+              controller: controller,
+            );
           }
 
           return AlertDialog(
@@ -81,51 +65,36 @@ Widget _alertContentInitial({
           ),
         ),
         SizedBox(height: 30),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            RaisedButton(
-              child: Text(
-                'Close',
-                style: TextStyle(fontSize: 14),
-              ),
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            RaisedButton(
-              child: Text(
-                'Reset Password',
-                style: TextStyle(fontSize: 14),
-              ),
-              elevation: 0,
-              textColor: Colors.white,
-              color: Theme.of(context).primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  controller.resetPassword();
-                }
-              },
-            ),
-          ],
-        )
+        RaisedButton(
+          child: Text(
+            'Reset Password',
+            style: TextStyle(fontSize: 14),
+          ),
+          elevation: 0,
+          textColor: Colors.white,
+          color: Theme.of(context).primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+          ),
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              controller.resetPassword();
+            }
+          },
+        ),
       ],
     ),
   );
 }
 
-Widget _alertContentWaiting({@required BuildContext context}) {
+Widget _alertContentFinish({
+  @required BuildContext context,
+  @required LandingPageRecoverPasswordDialogController controller,
+}) {
   return SingleChildScrollView(
     child: Column(
       children: [
-        Text("Please, check your inbox"),
+        Text(controller.message),
         SizedBox(height: 30),
         RaisedButton(
           child: Text(
@@ -136,62 +105,8 @@ Widget _alertContentWaiting({@required BuildContext context}) {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(18.0),
           ),
-          onPressed: () {},
-        ),
-      ],
-    ),
-  );
-}
-
-Widget _alertContentSuccess({
-  @required BuildContext context,
-  @required LandingPageRecoverPasswordDialogController controller,
-}) {
-  return SingleChildScrollView(
-    child: Column(
-      children: [
-        Text("Password has been reset succesfully"),
-        SizedBox(height: 30),
-        RaisedButton(
-          child: Text(
-            'Close',
-            style: TextStyle(fontSize: 14),
-          ),
-          elevation: 0,
-          textColor: Colors.white,
-          color: Theme.of(context).primaryColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
-          ),
           onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ],
-    ),
-  );
-}
-
-Widget _alertContentFailed({
-  @required BuildContext context,
-  @required LandingPageRecoverPasswordDialogController controller,
-}) {
-  return SingleChildScrollView(
-    child: Column(
-      children: [
-        Text("An error has ocurred"),
-        SizedBox(height: 30),
-        RaisedButton(
-          child: Text(
-            'Close',
-            style: TextStyle(fontSize: 14),
-          ),
-          elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
+            controller.retry();
           },
         ),
       ],
